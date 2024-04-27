8d42739e14ccdf098e122cb01aefad82d5d9a0bf
Yakimov Roman <85906769+Littump@users.noreply.github.com>
2024-04-27 23:16:35 +0300

    Back

---
+++ workers/recsystem/rec_system.py
@@ -0,0 +1,11 @@
+from base_worker import Worker
+
+class RecSystemWorker(Worker):
+    def __init__(self):
+        super(RecSystemWorker, self).__init__()
+        ...
+
+    def get_...(self, ...) -> float:
+
+    def _process(self):
+        ...
