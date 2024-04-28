7637c03aac7e7a83bc6506e11847788562c304d4
p0ntific <ddsjs0626@gmail.com>
2024-04-28 10:38:46 +0300

    fix avatars

--- frontend/src/modules/Navbar/ui/index.tsx
+++ frontend/src/modules/Navbar/ui/index.tsx
@@ -1,4 +1,3 @@
-import user from "@/assets/user.png";
 import { useNavigate } from "react-router-dom";
 interface Props {
   title?: string;
@@ -25,7 +24,10 @@ const Navbar = ({ title }: Props) => {
           className="btn btn-ghost btn-circle avatar"
         >
           <div className="w-10 rounded-full">
-            <img alt="Tailwind CSS Navbar component" src={user} />
+            <img
+              alt="Tailwind CSS chat bubble component"
+              src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+            />
           </div>
         </div>
         <ul
